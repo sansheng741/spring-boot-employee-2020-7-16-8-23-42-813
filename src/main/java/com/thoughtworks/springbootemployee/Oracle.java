cb43552f45898b6bbf59b283eb1d8092d7507f10
sansheng741 <807156237@qq.com>
2020-07-28 17:12:47 +0800

    [spring-boot-employee] [colin & oliver] feature : create virtual databases

---
+++ src/main/java/com/thoughtworks/springbootemployee/Oracle.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.springbootemployee;
+
+import com.thoughtworks.springbootemployee.entity.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by CHENCO9 on 7/28/2020 5:11 PM
+ */
+public class Oracle {
+    List<Employee> employeeList = new ArrayList<>();
+}
