75eb4073c9e1d60d896de119a1c72d5799743d20
sansheng741 <807156237@qq.com>
2020-07-28 17:07:30 +0800

    [spring-boot-employee] [colin & oliver] feature : create EmployeeController

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by CHENCO9 on 7/28/2020 4:59 PM
+ */
+@RestController
+public class EmployeeController {
+
+
+
+}
